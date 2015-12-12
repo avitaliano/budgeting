5f87847a9782edd83e31b5672258a0cd5bd5b106
Arnaldo Vitaliano <vitaliano@gmail.com>
2015-12-12 13:57:42 -0200

    creating/loading/writing procedures to classes.csv file

--- budget_analyse.R
+++ budget_analyse.R
@@ -9,31 +9,60 @@ options(scipen=999)
 # setting wd
 csv_basepath = "~/Documents/SpiderOak/Financeiro/ofx/"
 
-# lists csv files
-csv_list <- paste( csv_basepath, list.files(pattern = "*.csv", path = csv_basepath, recursive = TRUE), sep = '')
+load_ofx_flat <- function(basedir){
+        # lists csv files
+        csv_list <- paste( csv_basepath, list.files(pattern = "*.csv", path = csv_basepath, recursive = TRUE), sep = '')
 
-colClasses <- c('integer','string','string','string','string','string','factor','double','factor',
+        colClasses <- c('integer','string','string','string','string','string','factor','double','factor',
                 'date', 'date', 'string','string','date', 'double',  'string', 'factor','string')
 
-# loads csv into data frames
-dt = do.call(rbind, lapply(csv_list, fread, colClasses = colClasses))
+        # loads csv into data frames
+        dt = do.call(rbind, lapply(csv_list, fread, colClasses = colClasses))
 
-# loads classification file
-classes = read.csv("classes.csv", header = TRUE, sep = ";", stringsAsFactors = FALSE)
+        # cast date columns to date
+        dt$stat.start_date <- as.Date(dt$stat.start_date)
+        dt$stat.end_date <- as.Date(dt$stat.end_date)
+        dt$t.date <- as.Date(dt$t.date)
+        
+        dt[, t.month := as.Date(strftime(t.date, "%Y-%m-01")) ]
+        
+        dt
+}
 
-# cast date columns to date
-dt$stat.start_date <- as.Date(dt$stat.start_date)
-dt$stat.end_date <- as.Date(dt$stat.end_date)
-dt$t.date <- as.Date(dt$t.date)
+load_classes <- function(classes_file){
+        
+        # tests if classes file exists
+        if ( file.exists(classes_file)){
+                # loads classification file
+                classes = fread(classes_file, header = TRUE, sep = ";", stringsAsFactors = FALSE)
+        } else{
+                # creates empty classes file
+                warning(paste("file ", classes_file, " does not exist. creating one."))
+                classes <- data.table(match_type = character(), class = character(), 
+                                      subclass = character(), match_expression = character(), match_amount= double())
+        }
+        
+        classes
+}
 
-dt[, t.month := as.Date(strftime(t.date, "%Y-%m-01")) ]
+update_classes <- function(classes, in_match_type, in_class, in_subclass, in_expression, in_amount = NA){
+        new_classes <- rbind(classes, data.table(match_type=in_match_type,
+                                  class=in_class, subclass = in_subclass, 
+                                  match_expression=in_expression,
+                                  match_amount=in_amount))
+        new_classes[order(match_type, class, subclass, match_expression)]
+}
+
+write_classes <- function(classes, filename){
+        write.table(classes, filename, sep = ';', row.names = FALSE)
+}
 
 classify_transaction_textual <- function(dt, classes){
         
-        classes <- classes[classes$match_type == "textual",]
+        classes <- classes[match_type == "textual",]
         for (i in 1:nrow(classes)){
-                dt[ str_detect(t.memo, classes[i,4]), t.class := classes[i,2]]
-                dt[ str_detect(t.memo, classes[i,4]), t.subclass := classes[i,3]]
+                dt[ str_detect(t.memo, classes[i,match_expression]), t.class := classes[i,class]]
+                dt[ str_detect(t.memo, classes[i,match_expression]), t.subclass := classes[i,subclass]]
         }
         
         dt
@@ -43,13 +72,33 @@ classify_transaction_amount <- function(dt, classes){
         
         classes <- classes[classes$match_type == "amount",]
         for (i in 1:nrow(classes)){
-                dt[ str_detect(t.memo, classes[i,4]) & t.amount == classes[i,5], 
-                    t.class := classes[i,2]]
-                dt[ str_detect(t.memo, classes[i,4]) & t.amount == classes[i,5], 
-                    t.subclass := classes[i,3]]
+                dt[ str_detect(t.memo, classes[i,match_expression]) & t.amount == classes[i,match_amount], 
+                    t.class := classes[i,class]]
+                dt[ str_detect(t.memo, classes[i,match_expression]) & t.amount == classes[i,match_amount], 
+                    t.subclass := classes[i,subclass]]
         }
         
         dt
+}
+
+classify_all_transactions <- function(dt){
+        # classfies generic classes
+        dt[, t.class := 'sem categoria']
+        dt[ str_detect(t.memo, "Cheque Compensado"), t.class := 'Outros']
+        dt[ str_detect(t.memo, "Cheque Compensado"), t.subclass := "Pagamentos"]
+        dt[ str_detect(t.memo, "Pagamento de Título"), t.class := 'Outros']
+        dt[ str_detect(t.memo, "Pagamento de Título"), t.subclass := 'Pagamentos']
+        dt[ str_detect(t.memo, "Pagto via Auto-Atend"), t.class := 'Outros']
+        dt[ str_detect(t.memo, "Pagto via Auto-Atend"), t.subclass := 'Pagamentos']
+        dt[ str_detect(t.memo, "Transferência"), t.class := 'Outros']
+        dt[ str_detect(t.memo, "Transferência"), t.subclass := 'Transferencias']
+        
+        # classifies expends/incomes
+        dt <- classify_transaction_textual(dt, classes)
+        dt <- classify_transaction_amount(dt, classes)
+        
+        # reclassify "others" incomes
+        #dt[t.class == "Outros" & t.amount > 0, t.class := "Entradas"][ t.subclass := "Transferencias"]
 }
 
 plot_expenses_by_class <- function(dt, in_year, in_currency){
@@ -124,31 +173,9 @@ plot_expenses_by_subclass <- function(dt, in_year, in_currency, in_class){
         pie(slices, labels = lbls, main = main, col=rainbow(length(lbls)))
 }
 
-
-classify_all_transactions <- function(dt){
-        # classfies generic classes
-        dt[, t.class := 'sem categoria']
-        dt[ str_detect(t.memo, "Cheque Compensado"), t.class := 'Outros']
-        dt[ str_detect(t.memo, "Cheque Compensado"), t.subclass := "Pagamentos"]
-        dt[ str_detect(t.memo, "Pagamento de Título"), t.class := 'Outros']
-        dt[ str_detect(t.memo, "Pagamento de Título"), t.subclass := 'Pagamentos']
-        dt[ str_detect(t.memo, "Pagto via Auto-Atend"), t.class := 'Outros']
-        dt[ str_detect(t.memo, "Pagto via Auto-Atend"), t.subclass := 'Pagamentos']
-        dt[ str_detect(t.memo, "Transferência"), t.class := 'Outros']
-        dt[ str_detect(t.memo, "Transferência"), t.subclass := 'Transferencias']
-
-        # classifies expends/incomes
-        dt <- classify_transaction_textual(dt, classes)
-        dt <- classify_transaction_amount(dt, classes)
-
-        # reclassify "others" incomes
-        #dt[t.class == "Outros" & t.amount > 0, t.class := "Entradas"][ t.subclass := "Transferencias"]
-}
-
-
-#dt[ t.class == 'sem categoria' & t.date > '2015-01-01', .(t.month, t.memo, t.class, t.amount)][order(t.month)]
-
+# init procedures
+classes <- load_classes("classes.csv")
+dt <- load_ofx_flat(csv_basepath)
 dt <- classify_all_transactions(dt)
 
 plot_expenses_by_class(dt, 2015, "BRL")
-dt[year(t.date) == 2015 & t.class == "Outros", .(t.memo, t.amount, t.date, t.subclass)][order(t.date)]
